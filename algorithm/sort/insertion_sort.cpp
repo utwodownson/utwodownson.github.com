4d40460bb96b7be736e44f1ebd8d71b83071376b
Yang Shuang <utwodownson@gmail.com>
2013-03-12 15:03:39 +0800

    Update insertion_sort.cpp

--- algorithm/sort/insertion_sort.cpp
+++ algorithm/sort/insertion_sort.cpp
@@ -17,22 +17,3 @@ void insertion_sort(int *data, int len)
     }
 }
 
-/*
-int main()
-{
-    int number = 0;
-    scanf("%d", &number);
-    int data[100] = {0};
-    for (int i = 0; i < number; ++i)
-    {
-        scanf("%d", &data[i]);
-    }
-    insertion_sort(data, number);
-    for (int i = 0; i < number; ++i)
-    {
-        printf("%d ", data[i]);
-    }
-    printf("\n");
-}
-*/
-
